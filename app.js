7509f302c5794c7a27446551eeea5f861311c6a3
lamsonhai1379 <lamsonhai1379@gmail.com>
2019-09-23 14:48:50 +0700

    fix undedined when close openDirectory

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const { dialog } = require("electron").remote;
 var folderAddress = document.querySelector('.folder-address');
 var btnBrowseFolder = document.querySelector(".btn_browse_folder");
 var body = document.querySelector('body')
-let node = document.createElement('span')
-let text = document.createTextNode('Test')
-node.appendChild(text)
 
 
 let options = {
@@ -30,16 +27,47 @@ let options = {
 let folder = []
 
 btnBrowseFolder.addEventListener('click', () => {
-    dialog.showOpenDialog(options).then(result => {
 
-        folderAddress.setAttribute('value', result.filePaths[0])
-        folder.push({ folderPath: result.filePaths[0], folderName: result.filePaths[0].match(/(\w+)/g)[result.filePaths[0].match(/(\w+)/g).length - 1], files: this.getFileFromPath(result.filePaths[0]) })
-        console.log(folder);
-    })
+    if (folder.length <= 4) {
+        dialog.showOpenDialog(options).then(result => {
+            if (result.filePaths[0] != undefined) {
+                folderAddress.setAttribute('value', result.filePaths[0])
+                folder.push({ folderPath: result.filePaths[0], folderName: path.basename(result.filePaths[0]), files: this.getFileFromPath(result.filePaths[0]) })
+                this.displayFolderName(folder)
+            }
+
+        }).catch(err => dialog.showErrorBox('ERROR', err))
+    } else {
+        dialog.showErrorBox("ERROR", "Maximum 5 folder")
+    }
 
 });
 
 
+displayFolderName = folder => {
+
+
+    if (folder) {
+
+        let div = document.createElement('div');
+        div.setAttribute('class', 'show-folders')
+
+        folder.forEach(value => {
+            let tag = document.createElement('span');
+            let text = document.createTextNode(value.folderName);
+            tag.setAttribute('class', value.folderName)
+            tag.setAttribute('title', 'click to remove')
+            tag.setAttribute('onclick', 'removeFolder(event)')
+            tag.appendChild(text)
+            div.appendChild(tag)
+        })
+        body.appendChild(div)
+    }
+}
+
+removeFolder = event => {
+    console.log(event.target.className);
+}
 
 getFileFromPath = filePath => {
     let files = [];
