ac159cc0a9122e56180b19a12c7018d59c1a1b28
lamsonhai1379 <lamsonhai1379@gmail.com>
2019-09-23 13:35:48 +0700

    get folder detail

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const { dialog } = require("electron").remote;
 
 var folderAddress = document.querySelector('.folder-address');
 var btnBrowseFolder = document.querySelector(".btn_browse_folder");
-var showFolders = document.getElementById('display-folder')
-console.log(showFolders);
+var body = document.querySelector('body')
+let node = document.createElement('span')
+let text = document.createTextNode('Test')
+node.appendChild(text)
+
+
 let options = {
     // See place holder 1 in above image
     title: "Choose folder has file you want to delete",
@@ -23,24 +27,27 @@ let options = {
 
 //open directory
 
+let folder = []
 
 btnBrowseFolder.addEventListener('click', () => {
     dialog.showOpenDialog(options).then(result => {
 
-        this.getFileFromPath(result.filePaths[0]);
-
-
+        folderAddress.setAttribute('value', result.filePaths[0])
+        folder.push({ folderPath: result.filePaths[0], folderName: result.filePaths[0].match(/(\w+)/g)[result.filePaths[0].match(/(\w+)/g).length - 1], files: this.getFileFromPath(result.filePaths[0]) })
+        console.log(folder);
     })
 
 });
-let files = [];
+
+
+
 getFileFromPath = filePath => {
-    if (filePath !== undefined) {
-        folderAddress.setAttribute('value', filePath);
-        fs.readdir(filePath, (err, file) => {
-            files = file.length > 1 ? [...file] : []
+    let files = [];
 
-        })
 
-    }
+    fs.readdir(filePath, (err, file) => {
+        files.push(file)
+
+    })
+    return files
 }
